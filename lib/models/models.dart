d5a571a211c216726bc66a6bb7e12afd1f5bfb30
NahomTorba <nahumtorba@gmail.com>
2025-12-31 15:13:41 +0300

    supplier model

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -24,3 +24,33 @@ class User {
     };
   }
 }
+
+class Supplier {
+  final int? id;
+  final String name;
+  final String? phone;
+  final String? email;
+  final String? address;
+
+  Supplier({this.id, required this.name, this.phone, this.email, this.address});
+
+  factory Supplier.fromMap(Map<String, dynamic> map) {
+    return Supplier(
+      id: map['Supplier_ID'],
+      name: map['Supplier_Name'],
+      phone: map['Phone'],
+      email: map['Email'],
+      address: map['Address'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'Supplier_ID': id,
+      'Supplier_Name': name,
+      'Phone': phone,
+      'Email': email,
+      'Address': address,
+    };
+  }
+}
