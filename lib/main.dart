b07436beccc6953ff8ed258b9e66cde884c6e35d
NahomTorba <nahumtorba@gmail.com>
2026-01-10 23:57:07 +0300

    add UI to test dao methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:shop_ims/pages/home_page.dart';
+import 'package:shop_ims/pages/user_page.dart';
+import 'package:shop_ims/pages/user_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: HomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: UserPage(),
     );
   }
 }
