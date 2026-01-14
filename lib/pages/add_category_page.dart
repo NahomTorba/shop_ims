5cfc6e2f240e0d6449c74f8d36c20bb18f6bfb28
NahomTorba <nahumtorba@gmail.com>
2026-01-14 22:19:40 +0300

    fix dart analysis problems

--- lib/pages/add_category_page.dart
+++ lib/pages/add_category_page.dart
@@ -67,7 +67,7 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
                 borderRadius: BorderRadius.circular(12),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
+                    color: Colors.black.withValues(alpha: 0.05),
                     blurRadius: 10,
                     offset: const Offset(0, 4),
                   ),
